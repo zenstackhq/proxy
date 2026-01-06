46e9e312410a5111d956b14b2a0329344154df9a
jiasheng <jiashengguo@outlook.com>
2026-01-06 12:01:17 +0800

    initial commit

---
+++ src/utils/version-utils.ts
@@ -0,0 +1,58 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+export function getVersion(): string | undefined {
+  try {
+    return require('../package.json').version
+  } catch {
+    try {
+      // dev environment
+      return require('../../package.json').version
+    } catch {
+      return undefined
+    }
+  }
+}
+
+export function getNodeModulesFolder(startPath?: string): string | undefined {
+  startPath = startPath ?? process.cwd()
+  if (startPath.endsWith('node_modules')) {
+    return startPath
+  } else if (fs.existsSync(path.join(startPath, 'node_modules'))) {
+    return path.join(startPath, 'node_modules')
+  } else if (startPath !== '/') {
+    const parent = path.join(startPath, '..')
+    return getNodeModulesFolder(parent)
+  } else {
+    return undefined
+  }
+}
+
+export function getZenStackVersion(): string | undefined {
+  try {
+    return require('zenstack').version
+  } catch {
+    try {
+      // runtime
+      return require('@zenstackhq/runtime/package.json').version
+    } catch {
+      return undefined
+    }
+  }
+}
+
+/**
+ * Gets the installed Prisma's version
+ */
+export function getPrismaVersion(): string | undefined {
+  try {
+    return require('@prisma/client/package.json').version
+  } catch {
+    try {
+      return require('prisma/package.json').version
+    } catch {
+      return undefined
+    }
+  }
+}
